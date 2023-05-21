faf6d6fab9549a906f91233366b22285a891d593
Suresh Kumar <kumarsuresh1525@gmail.com>
2023-05-21 18:02:06 +0530

    added swagger configuration

--- packages/server/src/controller/book.controller.ts
+++ packages/server/src/controller/book.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateBookDto } from 'src/dto/book/book.dto';
 import { BookService } from 'src/service/book.service';
 import { TSearch } from 'src/types/common.type';
 
 @Controller('books')
+@ApiTags('Books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './module/app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -6,7 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
